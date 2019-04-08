6fd44d7c5697572269472341114e3c836ee0c4aa
Marco Monter <monter.marco@gmail.com>
2019-04-07 23:39:51 -0500

    make it better

--- src/components/site/Header.js
+++ src/components/site/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { Menu, Button } from 'antd';
-import { Link, Redirect } from 'react-router-dom';
+import { Menu } from 'antd';
+import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../../actions'  
 
